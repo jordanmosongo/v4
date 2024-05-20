6ac4da244596bc125dab2f1dbfca3c713ef651c8
jordanmosongo <76458271+jordanmosongo@users.noreply.github.com>
2024-05-20 14:04:27 +0100

    integrate limitless template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import '../../public/assets/fonts/inter/inter.css';
+import "../../public/assets/icons/phosphor/styles.min.css";
+import "@/app/assets/css/ltr/all.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script src="assets/js/vendor/visualization/d3/d3.min.js" defer />
+      <script src="assets/js/vendor/visualization/d3/d3_tooltip.js" defer />
+
+      <script src="assets/js/app.js" defer />
+      <script src="assets/demo/pages/dashboard.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/streamgraph.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/sparklines.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/lines.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/areas.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/donuts.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/bars.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/progress.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/heatmaps.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/pies.js" defer />
+      <script src="assets/demo/charts/pages/dashboard/bullets.js" defer />
     </html>
   );
 }
